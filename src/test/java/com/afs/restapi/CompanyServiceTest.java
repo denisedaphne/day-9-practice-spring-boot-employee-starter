0740fba61ac5f4bf68052b55ad9a2d56e598d3d2
Daphne Balane <balanedaphne20@gmail.com>
2023-08-24 20:32:51 +0800

    test: added should_return_company_when_get_by_id_given_company_service_and_company_id

--- src/test/java/com/afs/restapi/CompanyServiceTest.java
+++ src/test/java/com/afs/restapi/CompanyServiceTest.java
@@ -1,6 +1,7 @@
 package com.afs.restapi;
 
 import com.afs.restapi.entity.Company;
+import com.afs.restapi.exception.CompanyNotFoundException;
 import com.afs.restapi.repository.CompanyJpaRepository;
 import com.afs.restapi.repository.EmployeeJpaRepository;
 import com.afs.restapi.service.CompanyService;
@@ -10,9 +11,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
@@ -46,4 +47,19 @@ public class CompanyServiceTest {
         // Then
         assertEquals(companies, allCompanies);
     }
+
+    @Test
+    void should_return_company_when_get_by_id_given_company_service_and_company_id() {
+        // Given
+        Company company = new Company(1L, "OOCL");
+        when(companyJpaRepository.findById(company.getId())).thenReturn(Optional.of(company));
+
+        // When
+        Company foundCompany = companyService.findById(company.getId());
+
+        // Then
+        assertEquals(company, foundCompany);
+    }
+
+
 }
