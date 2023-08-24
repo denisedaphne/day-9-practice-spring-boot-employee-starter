8948ef0248b839a38d11fa8cd53ed99dc4fc3bd0
Daphne Balane <balanedaphne20@gmail.com>
2023-08-24 21:02:50 +0800

    feat: added repository annotation in CompanyJpaRepository class

--- src/main/java/com/afs/restapi/repository/CompanyJpaRepository.java
+++ src/main/java/com/afs/restapi/repository/CompanyJpaRepository.java
@@ -2,6 +2,8 @@ package com.afs.restapi.repository;
 
 import com.afs.restapi.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyJpaRepository extends JpaRepository<Company, Long> {
 }
