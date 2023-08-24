484dafb1eed602a0dbc0afa45d1d86b60e3db85a
Daphne Balane <balanedaphne20@gmail.com>
2023-08-24 20:51:25 +0800

    refactor: added SuppressWarnings for CompanyController class

--- src/main/java/com/afs/restapi/controller/CompanyController.java
+++ src/main/java/com/afs/restapi/controller/CompanyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SuppressWarnings("all")
 @RequestMapping("companies")
 @RestController
 public class CompanyController {
